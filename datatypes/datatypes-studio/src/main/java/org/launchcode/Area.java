7de4edd3c8416abff6fbaf5cdb33c65c297cc72f
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-08-21 19:28:46 -0500

    first commit, studio 1 unit 2

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,19 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static  void  main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a radius: ");
+        double radius = scanner.nextDouble();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is " + area);
+
+        scanner.close();
+
+    }
+}
