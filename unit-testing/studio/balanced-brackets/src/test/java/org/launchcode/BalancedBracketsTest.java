cf42e9260c0399b2a82777431fa364828eaddf6c
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-09-11 19:59:28 -0500

    New Test

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,67 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testSingleOpeningBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testSingleClosingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void testMultipleBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void testMultipleOpeningBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void testMultipleClosingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
+    @Test
+    public void testBracketsWithoutBracketCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch [Code]"));
+    }
+
+    @Test
+    public void testBracketsWithCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch Code]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testBracketsWithIntegers() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[100]"));
+    }
+
+    @Test
+    public void testBracketsWithNonBalancedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+
+
+
+
 }
+
